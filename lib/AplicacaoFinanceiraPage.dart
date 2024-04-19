13a5cb2f704aca6fa011f08379ac1b526cef37a6
gabRochaOliv <bielrocha2003@hotmail.com>
2024-04-19 00:54:39 -0300

    Atualizando algumas paginas para o padrão definido

--- lib/AplicacaoFinanceiraPage.dart
+++ lib/AplicacaoFinanceiraPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class AplicacaoFinanceiraPage extends StatefulWidget {
   const AplicacaoFinanceiraPage({Key? key}) : super(key: key);
@@ -8,14 +9,16 @@ class AplicacaoFinanceiraPage extends StatefulWidget {
       _AplicacaoFinanceiraPageState();
 }
 
-class _AplicacaoFinanceiraPageState extends State<AplicacaoFinanceiraPage> {
-  late int _selectedTabIndex;
+class _AplicacaoFinanceiraPageState extends State<AplicacaoFinanceiraPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
   late List<Widget> _tabContents;
 
   @override
   void initState() {
     super.initState();
-    _selectedTabIndex = 0;
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(_handleTabSelection);
     _tabContents = [
       Center(child: Text('Conteúdo da Tab 1')),
       Center(child: Text('Conteúdo da Tab 2')),
@@ -23,55 +26,78 @@ class _AplicacaoFinanceiraPageState extends State<AplicacaoFinanceiraPage> {
     ];
   }
 
+  void _handleTabSelection() {
+    setState(() {
+      // Atualiza o índice da aba selecionada
+      _selectedTabIndex = _tabController.index;
+    });
+  }
+
+  int _selectedTabIndex = 0; // Índice da aba selecionada
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: _tabContents.length,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Aplicação Financeira'),
-          backgroundColor: Theme.of(context).appBarTheme.backgroundColor ??
-              const Color.fromRGBO(33, 150, 243, 1),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.star),
-              onPressed: () {
-                // lógica para lidar com a ação do botão de estrela
-              },
-            ),
-          ],
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Sinal'),
-              Tab(
-                child: Text(
-                  'Língua\nPortuguesa',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Tab(text: 'Visual'),
-            ],
-            onTap: (index) {
-              setState(() {
-                _selectedTabIndex = index;
-              });
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Aplicação Financeira'),
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor ??
+            const Color.fromRGBO(33, 150, 243, 1),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.star),
+            onPressed: () {
+              // lógica para lidar com a ação do botão de estrela
             },
-            indicatorSize: TabBarIndicatorSize
-                .label, // Exibe o indicador do tamanho do rótulo
-            labelStyle: TextStyle(
-              fontWeight: FontWeight.bold, // Define o peso da fonte
-              fontSize: 17, // Define o tamanho da fonte
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            color: Color.fromARGB(255, 244, 243, 243),
+            child: Center(
+              child: TabBar(
+                controller: _tabController,
+                isScrollable: false,
+                tabs: [
+                  Tab(
+                    text: 'Sinal',
+                    // child: Text(
+                    //   'Sinal',
+                    //   textAlign: TextAlign.center,
+                    // ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'Língua\nPortuguesa',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Tab(
+                    text: 'Visual',
+                    // child: Text(
+                    //   'Visual',
+                    //   textAlign: TextAlign.center,
+                    // ),
+                  ),
+                ],
+                labelStyle: TextStyle(
+                  fontSize: 17, // Define o tamanho da fonte
+                ),
+                labelPadding: EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorColor: Color.fromARGB(255, 0, 140, 255),
+                unselectedLabelColor: Color.fromRGBO(64, 64, 64, 1),
+                labelColor: Color.fromRGBO(33, 150, 243, 1),
+              ),
             ),
-            indicatorColor: Color.fromARGB(
-                255, 77, 77, 77), // Cor de fundo quando selecionado
-            unselectedLabelColor: Color.fromARGB(255, 222, 222, 222),
-            labelColor: Color.fromARGB(
-                255, 255, 255, 255), // Cor do texto não selecionado
           ),
         ),
-        body: TabBarView(
-          children: _tabContents,
-        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: _tabContents,
       ),
     );
   }
--- lib/BalancoContabilPage.dart
+++ lib/BalancoContabilPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class BalancoContabilPage extends StatefulWidget {
   const BalancoContabilPage({Key? key}) : super(key: key);
@@ -7,59 +8,122 @@ class BalancoContabilPage extends StatefulWidget {
   State<BalancoContabilPage> createState() => _BalancoContabilPageState();
 }
 
-class _BalancoContabilPageState extends State<BalancoContabilPage> {
-  late int _selectedTabIndex;
+class _BalancoContabilPageState extends State<BalancoContabilPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
   late List<Widget> _tabContents;
+
+  VideoPlayerController? _videoController;
+  Future<void>? _initializeVideoPlayerFuture;
 
   @override
   void initState() {
     super.initState();
-    _selectedTabIndex = 0;
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(_handleTabSelection);
     _tabContents = [
-      Center(child: Text('Conteúdo da Tab 1')),
+      _buildVideoPlayerTab(),
       Center(child: Text('Conteúdo da Tab 2')),
       Center(child: Text('Conteúdo da Tab 3')),
     ];
+
+    _initializeVideoPlayer();
+  }
+
+  void _initializeVideoPlayer() async {
+    _videoController = VideoPlayerController.asset(
+      'assets/bemvindo.mp4',
+    );
+
+    await _videoController!.initialize();
+    setState(() {}); // Atualiza o estado após a inicialização do vídeo
+    _videoController!
+        .play(); // Inicia a reprodução do vídeo após a inicialização
+    _videoController!.setLooping(true); // Define a reprodução em loop
+  }
+
+  Widget _buildVideoPlayerTab() {
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Center(
+            child: AspectRatio(
+              aspectRatio: _videoController!.value.aspectRatio,
+              child: VideoPlayer(_videoController!),
+            ),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  void _handleTabSelection() {
+    setState(() {
+      _selectedTabIndex = _tabController.index;
+    });
   }
 
   @override
+  void dispose() {
+    _tabController.dispose();
+    _videoController?.dispose();
+    super.dispose();
+  }
+
+  int _selectedTabIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: _tabContents.length,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Balanço Contábil'),
-          backgroundColor: Theme.of(context).appBarTheme.backgroundColor ??
-              const Color.fromRGBO(33, 150, 243, 1),
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Tab 1'),
-              Tab(
-                child: Text(
-                  'Língua\nPortuguesa',
-                  textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Balanço Contábil'),
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor ??
+            const Color.fromRGBO(33, 150, 243, 1),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            color: Color.fromARGB(255, 244, 243, 243),
+            child: Center(
+              child: TabBar(
+                controller: _tabController,
+                isScrollable: false,
+                tabs: [
+                  Tab(
+                    child: Text(
+                      'Sinal',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'Língua\nPortuguesa',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'Visual',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+                labelStyle: TextStyle(
+                  fontSize: 18,
+                ),
+                labelPadding: EdgeInsets.symmetric(
+                  horizontal: 15,
                 ),
               ),
-              Tab(text: 'Tab 3'),
-            ],
-            onTap: (index) {
-              setState(() {
-                _selectedTabIndex = index;
-              });
-            },
-            indicatorSize: TabBarIndicatorSize.label,
-            labelStyle: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 17,
             ),
-            indicatorColor: const Color.fromARGB(255, 77, 77, 77),
-            unselectedLabelColor: const Color.fromARGB(255, 222, 222, 222),
-            labelColor: const Color.fromARGB(255, 255, 255, 255),
           ),
         ),
-        body: TabBarView(
-          children: _tabContents,
-        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: _tabContents,
       ),
     );
   }
