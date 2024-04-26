3cda096e1d1cfc3343dd86525388fb840f987059
gabRochaOliv <bielrocha2003@hotmail.com>
2024-04-26 12:28:08 -0300

    Erro do carregamento corrigido

--- lib/BalancoContabilPage.dart
+++ lib/BalancoContabilPage.dart
@@ -12,20 +12,15 @@ class _BalancoContabilPageState extends State<BalancoContabilPage>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
   late List<Widget> _tabContents;
-
   late VideoPlayerController _videoController;
-  Future<void>? _initializeVideoPlayerFuture;
+
+  int _selectedTabIndex = 0;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
     _tabController.addListener(_handleTabSelection);
-    _tabContents = [
-      _buildVideoPlayerTab(),
-      Center(child: Text('Conteúdo da Tab 2')),
-      Center(child: Text('Conteúdo da Tab 3')),
-    ];
     _videoController = VideoPlayerController.asset(
       'assets/bemvindo.mp4',
     );
@@ -33,30 +28,25 @@ class _BalancoContabilPageState extends State<BalancoContabilPage>
     _initializeVideoPlayer();
   }
 
-  void _initializeVideoPlayer() async {
-    await _videoController!.initialize();
+  Future<void> _initializeVideoPlayer() async {
+    await _videoController.initialize();
     setState(() {}); // Atualiza o estado após a inicialização do vídeo
-    _videoController!
+    await _videoController
         .play(); // Inicia a reprodução do vídeo após a inicialização
-    _videoController!.setLooping(true); // Define a reprodução em loop
+    await _videoController.setLooping(true); // Define a reprodução em loop
   }
 
   Widget _buildVideoPlayerTab() {
-    return FutureBuilder(
-      future: _initializeVideoPlayerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Center(
-            child: AspectRatio(
-              aspectRatio: _videoController!.value.aspectRatio,
-              child: VideoPlayer(_videoController!),
-            ),
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
-    );
+    if (_videoController.value.isInitialized) {
+      return Center(
+        child: AspectRatio(
+          aspectRatio: _videoController.value.aspectRatio,
+          child: VideoPlayer(_videoController),
+        ),
+      );
+    } else {
+      return const Center(child: CircularProgressIndicator());
+    }
   }
 
   void _handleTabSelection() {
@@ -68,28 +58,31 @@ class _BalancoContabilPageState extends State<BalancoContabilPage>
   @override
   void dispose() {
     _tabController.dispose();
-    _videoController?.dispose();
+    _videoController.dispose();
     super.dispose();
   }
 
-  int _selectedTabIndex = 0;
-
   @override
   Widget build(BuildContext context) {
+    _tabContents = [
+      _buildVideoPlayerTab(),
+      const Center(child: Text('Conteúdo da Tab 2')),
+      const Center(child: Text('Conteúdo da Tab 3')),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Balanço Contábil'),
         backgroundColor: Theme.of(context).appBarTheme.backgroundColor ??
             const Color.fromRGBO(33, 150, 243, 1),
         bottom: PreferredSize(
-          preferredSize: Size.fromHeight(kToolbarHeight),
+          preferredSize: const Size.fromHeight(kToolbarHeight),
           child: Container(
-            color: Color.fromARGB(255, 244, 243, 243),
+            color: const Color.fromARGB(255, 244, 243, 243),
             child: Center(
               child: TabBar(
                 controller: _tabController,
                 isScrollable: false,
-                tabs: [
+                tabs: const [
                   Tab(
                     child: Text(
                       'Sinal',
@@ -109,10 +102,10 @@ class _BalancoContabilPageState extends State<BalancoContabilPage>
                     ),
                   ),
                 ],
-                labelStyle: TextStyle(
+                labelStyle: const TextStyle(
                   fontSize: 18,
                 ),
-                labelPadding: EdgeInsets.symmetric(
+                labelPadding: const EdgeInsets.symmetric(
                   horizontal: 15,
                 ),
               ),
